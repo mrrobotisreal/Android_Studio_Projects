b13a762aa68290fba85ee79bace0e173f32c1c24
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-27 22:16:58 -0600

    Working on typing, specifically with integers currently.

--- KotlinBasics/app/src/main/java/com/iamwintrow/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/iamwintrow/kotlinbasics/Basics.kt
@@ -1,7 +1,22 @@
 package com.iamwintrow.kotlinbasics
 
 fun main() {
+    // mutable variable
     var myName = "Mitchell"
+    // immutable variable
     val myLastName = "Wintrow"
+    // TODO: Print both variables
+    // Type int (32 bit)
+    var myAge = 31
+
+    // Integer Types: Byte (8 bit), Short (16 bit), Int (32 bit), Long (64 bit)
+    // type Byte
+    val myByte: Byte = 13
+    // type Short
+    val myShort: Short = 125
+    // type Int
+    val myInt: Int = 123123123
+    // type Long
+    val myLong: Long = 12_039_812_309_487_120
     print("Hello $myName $myLastName!")
 }
