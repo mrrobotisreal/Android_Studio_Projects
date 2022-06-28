e8471cd8fb91b05b2e49aeae293d52f9460850fc
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-27 22:21:09 -0600

    Just worked with floats and doubles.

--- KotlinBasics/app/src/main/java/com/iamwintrow/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/iamwintrow/kotlinbasics/Basics.kt
@@ -18,5 +18,9 @@ fun main() {
     val myInt: Int = 123123123
     // type Long
     val myLong: Long = 12_039_812_309_487_120
+
+    // Floating Point number types: Float (32 bit), Double (64 bit)
+    val myFloat: Float = 13.37F
+    val myDouble: Double = 3.14159265358979323846
     print("Hello $myName $myLastName!")
 }
