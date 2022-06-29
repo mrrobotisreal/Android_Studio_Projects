45653023975117af625b1ef7facb0dd584d6312d
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-28 21:51:51 -0600

    Added linear gradient background.

--- DOBCalc/app/src/main/java/com/iamwintrow/dobcalc/MainActivity.kt
+++ DOBCalc/app/src/main/java/com/iamwintrow/dobcalc/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        title = "Age In Minutes"
 
         val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
         tvSelectedDate = findViewById<TextView>(R.id.tvSelectedDate)
