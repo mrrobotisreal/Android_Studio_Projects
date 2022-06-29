423dd16c292d53e37c5364da1536164245a182e5
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-28 18:28:29 -0600

    Worked with .. spread type operator.

--- KotlinBasics/app/src/main/java/com/iamwintrow/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/iamwintrow/kotlinbasics/Basics.kt
@@ -60,4 +60,13 @@ fun main() {
             println("Hello anonymous person")
         }
     }
+
+    var month = 3
+    when (month) {
+        in 3..5 -> println("Spring")
+        in 6..8 -> println("Summer")
+        in 9..11 -> println("Fall")
+        12, 1, 2 -> println("Winter")
+        else -> println("Invalid Season")
+    }
 }
