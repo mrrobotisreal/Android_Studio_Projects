2989c67d8b71f45d1c4ee72c74841750bfa0b2b8
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-29 22:15:10 -0600

    Filled out data class.

--- MyQuizApp/app/src/main/java/com/iamwintrow/myquizapp/Question.kt
+++ MyQuizApp/app/src/main/java/com/iamwintrow/myquizapp/Question.kt
@@ -1,3 +1,12 @@
 package com.iamwintrow.myquizapp
 
-data class Question()
+data class Question(
+    val id: Int,
+    val questions: String,
+    val image: Int,
+    val optionOne: String,
+    val optionTwo: String,
+    val optionThree: String,
+    val optionFour: String,
+    val correctAnswer: Int,
+)
