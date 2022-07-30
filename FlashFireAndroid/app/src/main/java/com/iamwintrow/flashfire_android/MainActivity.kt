f7b97d5929a9ae024a4494e96e862b83f7798d3a
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-07-29 20:03:16 -0600

    Added event listeners to register text and login button.

--- FlashFireAndroid/app/src/main/java/com/iamwintrow/flashfire_android/MainActivity.kt
+++ FlashFireAndroid/app/src/main/java/com/iamwintrow/flashfire_android/MainActivity.kt
@@ -4,17 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.SpannableString
 import android.text.style.UnderlineSpan
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     lateinit var textView : TextView
     lateinit var mainTitleString : SpannableString
+    lateinit var appCompatButton: Button
+    lateinit var registerButton: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         underLineMainTitle()
+        setEventHearerOnLoginButton()
+        setEventHearerOnRegisterButton()
     }
 
     fun underLineMainTitle() {
@@ -22,5 +28,19 @@ class MainActivity : AppCompatActivity() {
         mainTitleString = SpannableString(textView.text)
         mainTitleString.setSpan(UnderlineSpan(), 0, mainTitleString.length, 0)
         textView.text = mainTitleString
+    }
+
+    fun setEventHearerOnLoginButton() {
+        appCompatButton = findViewById(R.id.login_button)
+        appCompatButton.setOnClickListener {
+            val toast = Toast.makeText(applicationContext, "You logged in sucka!!!", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun setEventHearerOnRegisterButton() {
+        registerButton = findViewById(R.id.register_button)
+        registerButton.setOnClickListener {
+            val toast = Toast.makeText(applicationContext, "Thanks for registering!", Toast.LENGTH_LONG).show()
+        }
     }
 }
